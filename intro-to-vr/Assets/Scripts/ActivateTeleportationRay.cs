38f38e695fab24ca032d5c70d6e1380ffb7d269b
AlleywayNate <72818693+AlleywayNate@users.noreply.github.com>
2024-09-11 21:39:31 -0500

    Build out the rest of the Assignments

--- intro-to-vr/Assets/Scripts/ActivateTeleportationRay.cs
+++ intro-to-vr/Assets/Scripts/ActivateTeleportationRay.cs
@@ -2,18 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class ActivateTeleportationRay : MonoBehaviour
 {
     public GameObject leftTeleportationRay;
     public GameObject rightTeleportationRay;
 
-    public InputActionProperty leftActivateAction;
-    public InputActionProperty rightActivateAction;
+    public InputActionProperty leftActivate;
+    public InputActionProperty rightActivate;
+
+    public InputActionProperty leftCancel;
+    public InputActionProperty rightCancel;
+
+    public XRRayInteractor leftRay; 
+    public XRRayInteractor rightRay; 
 
     void Update ()
     {
-        leftTeleportationRay.SetActive(leftActivateAction.action.IsPressed());
-        rightTeleportationRay.SetActive(rightActivateAction.action.IsPressed());
+        bool isleftRayHovering = leftRay.TryGetHitInfo(out Vector3 leftHitPosition, out Vector3 leftNormal, out int leftNumber, out bool leftValid);
+        leftTeleportationRay.SetActive(isleftRayHovering && leftCancel.action.ReadValue<float>() == 0 && rightActivate.action.ReadValue<float>() == 0.1f);
+        
+        bool isRightRayHovering = rightRay.TryGetHitInfo(out Vector3 rightHitPosition, out Vector3 rightNormal, out int rightNmber, out bool rightValid);
+        rightTeleportationRay.SetActive(isRightRayHovering && rightCancel.action.ReadValue<float>() == 0 && leftActivate.action.ReadValue<float>() == 0.1f);
+
     }
 }
